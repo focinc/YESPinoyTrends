fafccb96617818a0ba0cb639135e9de250ae9406
MRN <mark.rencie.nol035@gmail.com>
2024-12-18 09:03:05 +0800

    init

---
+++ topbar.php
@@ -0,0 +1,31 @@
+<!-- topbar -->
+<div class="container-fluid">
+    <div class="row align-items-center bg-light px-lg-5">
+        <div class="col-12 col-md-8">
+            <div class="d-flex">
+                <div class="bg-primary text-white text-center py-2" style="width: 100px">
+                    Trending
+                </div>
+                <div class="owl-carousel owl-carousel-1 tranding-carousel position-relative d-inline-flex align-items-center ml-3"
+                    style="width: calc(100% - 200px); padding-left: 90px">
+                    <div class="text-truncate">
+                        <a class="text-secondary" href="">Free Wi-Fi hotspots have been set up across the city to
+                            improve internet access for residents.</a>
+                    </div>
+                    <div class="text-truncate">
+                        <a class="text-secondary" href="">A local cafe now offers free Wi-Fi to all customers,
+                            promoting a digital-friendly environment.</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 text-right d-none d-md-block">
+            <a href="./index.html" class="btn">Login</a>
+        </div>
+    </div>
+    <div class="d-flex justify-content-center align-items-center py-3 px-lg-5">
+        <div class="w-75 text-center text-lg-right rounded-3 overflow-hidden">
+            <img class="img-fluid" src="./img/ads-700x70.jpg" alt="" />
+        </div>
+    </div>
+</div>
